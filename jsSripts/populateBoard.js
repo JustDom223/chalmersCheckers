8edc31ee3281b2d8d732aa80aa315dfc63e076b5
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2023-11-30 12:57:49 +0100

    Fixed an error with my 2d array

--- jsSripts/populateBoard.js
+++ jsSripts/populateBoard.js
@@ -1,23 +1,3 @@
-function createBoard() {
-    const rows = 17; // Total rows in your HTML structure
-    const columns = 17; // Max columns at the widest part of the board
-
-    let board = [];
-
-    for (let i = 0; i < rows; i++) {
-        let row = [];
-        for (let j = 0; j < columns; j++) {
-            if (isValidCell(i, j)) {
-                row.push({}); // Empty cell
-            } else {
-                row.push(null); // Non-playable space
-            }
-        }
-        board.push(row);
-    }
-
-    return board;
-}
 
 function isValidCell(row, col) {
     // Logic to determine if a cell at (row, col) is a valid, playable cell
@@ -34,7 +14,6 @@ let board = [
     [null, null, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, null, null, null, null],
     [null, null, null, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, null, null, null, null],
     [null, null, null, null, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, null, null, null, null],
-    [null, null, null, null, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, null, null, null, null],
     [null, null, null, null, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, null, null, null],
     [null, null, null, null, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, null, null],
     [null, null, null, null, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, {  }, null],
@@ -43,6 +22,7 @@ let board = [
     [null, null, null, null, null, null, null, null, null, null, {  }, {  }, {  }, null, null, null, null],
     [null, null, null, null, null, null, null, null, null, null, null, {  }, {  }, null, null, null, null],
     [null, null, null, null, null, null, null, null, null, null, null, null, {  }, null, null, null, null],
+
    ];
 
 // for (i = 0; i < board.length; i++){
